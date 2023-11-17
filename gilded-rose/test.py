ec432c33734330b3dba0c16d99ec41a00b019d75
Théo FURST <tfurst@adflux.net>
2023-11-17 16:20:17 +0100

    Day one test working

--- gilded-rose/test.py
+++ gilded-rose/test.py
@@ -4,49 +4,33 @@ import unittest
 from modules.gilded_rose import Item, GildedRose
 
 
-test_cases_qual = {
+test_cases_one_day = {
     "it": [Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-        Item(name="Aged Brie", sell_in=2, quality=0),
-        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=50),
+        Item(name="Elixir of the Mongoose", sell_in=-5, quality=7),
         Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
         Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-        Item(name="Conjured Mana Cake", sell_in=3, quality=6)],
-    "expect": [19, 1, 6, 80, 80, 21, 50, 50, 4]
-}
-
-test_cases_sellin = {
-    "it": [Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-        Item(name="Aged Brie", sell_in=2, quality=0),
-        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-        Item(name="Conjured Mana Cake", sell_in=3, quality=6)],
-    "expect": [9, 1, 4, 0, -1, 14, 9, 4, 2]
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=30),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=10),
+        Item(name="Aged Brie", sell_in=3, quality=6),
+        Item(name="Aged Brie", sell_in=-3, quality=6)],
+    "expect_qual": [19, 49, 5, 80, 21, 32, 13, 7, 8],
+    "expect_sellin": [9, 9, -6, 0, 14, 9, 4, 2, -4]
 }
 
 class GildedRoseTest(unittest.TestCase):
     
     # Unit Test 1
     def test_quality(self):
-        gilded_rose = GildedRose(test_cases_qual["it"])
+        gilded_rose = GildedRose(test_cases_one_day["it"])
         gilded_rose.update_quality()
         
-        for i, expected in enumerate(test_cases_qual["expect"]):
+        for i, expected in enumerate(test_cases_one_day["expect_qual"]):
             self.assertEquals(expected, gilded_rose.items[i].quality)
             
-    # Unit Test 2
-    def test_sellin(self):
-        gilded_rose = GildedRose(test_cases_sellin["it"])
-        gilded_rose.update_quality()
+        for j, expected in enumerate(test_cases_one_day["expect_sellin"]):
+            self.assertEquals(expected, gilded_rose.items[j].sell_in)
         
-        for i, expected in enumerate(test_cases_sellin["expect"]):
-            self.assertEquals(expected, gilded_rose.items[i].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
