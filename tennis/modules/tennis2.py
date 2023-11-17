04087a869a040c26a1940059ab4a2366a6de7952
Théo FURST <tfurst@adflux.net>
2023-11-17 11:47:14 +0100

    Clearer variables

--- tennis/modules/tennis2.py
+++ tennis/modules/tennis2.py
@@ -14,12 +14,20 @@ class TennisGame2:
             self.p2 += 1
     
     def score(self):
+        
+        currently_winning = self.p1N if self.p1 > self.p2 else self.p2N
+        score = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+        
+        # Situation 1: . maximum score of 40 for everybody (p.1 and p.2 < 4)
+        #              . not yet at 40-40 (p1 + p2 < 6)
         if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
             p = ["Love", "Fifteen", "Thirty", "Forty"]
-            return p[self.p1] + "-All" if (self.p1 == self.p2) else p[self.p1] + "-" + p[self.p2]
+            return score[self.p1] + "-All" if (self.p1 == self.p2) else score[self.p1] + "-" + score[self.p2]
+        
+        # Situation 2: Either "Deuce", "Advantage" or "Win"
         else:
             if (self.p1 == self.p2):
                 return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
+            
             pointDiff = abs(self.p1-self.p2)
-            return "Advantage " + s if (pointDiff == 1) else "Win for " + s
+            return "Advantage " + currently_winning if (pointDiff == 1) else "Win for " + currently_winning
