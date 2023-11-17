4313d7202f99275b99f4b54752e294e33678dc65
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-17 14:46:04 +0100

    reorganized double filter

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -22,11 +22,10 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                if item.name not in ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]:
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
