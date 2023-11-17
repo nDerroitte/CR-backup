f0b07838be75af6eca549f0934548f21427b0f61
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-17 10:58:33 +0100

    victory condition

--- tennis/modules/tennis1.py
+++ tennis/modules/tennis1.py
@@ -23,16 +23,16 @@ class TennisGame1:
                 1: "Fifteen-All",
                 2: "Thirty-All",
             }.get(self.p1points, "Deuce")
+        elif (self.p1points >= 4 and self.p1points > self.p2points + 1):
+            result = "Win for " + self.player1Name
+        elif (self.p2points >= 4 and self.p2points > self.p1points + 1):
+            result = "Win for " + self.player2Name
         elif (self.p1points >= 4 or self.p2points >= 4):
             minusResult = self.p1points - self.p2points
             if (minusResult == 1):
                 result = "Advantage " + self.player1Name
             elif (minusResult == -1):
                 result = "Advantage " + self.player2Name
-            elif (minusResult >= 2):
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
         else:
             for i in range(1, 3):
                 if (i == 1):
