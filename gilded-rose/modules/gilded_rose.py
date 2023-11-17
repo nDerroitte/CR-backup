d76b0c895345c42f60efbfbe70879f1b18729348
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-17 14:47:39 +0100

    reorganizing if conds

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -22,10 +22,11 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name not in ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]:
+                if item.name not in ["Aged Brie",
+                                     "Backstage passes to a TAFKAL80ETC concert",
+                                     "Sulfuras, Hand of Ragnaros"]:
                     if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
