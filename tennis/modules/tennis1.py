0c03bbc866817d2db54031e8bf843a02bb3a09c0
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-17 13:22:36 +0100

    split in different function

--- tennis/modules/tennis1.py
+++ tennis/modules/tennis1.py
@@ -14,32 +14,44 @@ class TennisGame1:
         else:
             self.p2points += 1
 
+    def parity_scenarios(self):
+        if self.p1points < 3:
+            result = {
+                0: "Love-All",
+                1: "Fifteen-All",
+                2: "Thirty-All",
+            }[self.p1points]
+        else:
+            result = "Deuce"
+        return result
+
+    def classic_scenario(self):
+        conversion = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+        }
+        result = conversion[self.p1points] + "-" + conversion[self.p2points]
+        return result
+
+    def advantage_and_victory_scenario(self):
+        if (self.p1points == self.p2points + 1):
+            result = "Advantage " + self.player1Name
+        elif (self.p2points == self.p1points + 1):
+            result = "Advantage " + self.player2Name
+        elif (self.p1points > self.p2points + 1):
+            result = "Win for " + self.player1Name
+        else:
+            result = "Win for " + self.player2Name
+        return result
+
     def score(self):
         result = ""
         if (self.p1points == self.p2points):
-            if self.p1points < 3:
-                result = {
-                    0: "Love-All",
-                    1: "Fifteen-All",
-                    2: "Thirty-All",
-                }[self.p1points]
-            else:
-                result = "Deuce"
+            result = self.parity_scenarios()
         elif (self.p1points >= 4 or self.p2points >= 4):
-            if (self.p1points == self.p2points + 1):
-                result = "Advantage " + self.player1Name
-            elif (self.p2points == self.p1points + 1):
-                result = "Advantage " + self.player2Name
-            elif (self.p1points > self.p2points + 1):
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
+            result = self.advantage_and_victory_scenario()
         else:
-            conversion = {
-                0: "Love",
-                1: "Fifteen",
-                2: "Thirty",
-                3: "Forty",
-            }
-            result = conversion[self.p1points] + "-" + conversion[self.p2points]
+            result = self.classic_scenario()
         return result
