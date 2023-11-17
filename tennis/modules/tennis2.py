01e4cbc2319a2b13d30d8dc585f18c540482dc59
mdupret <maureen.dupret@businessdecision.be>
2023-11-17 13:31:38 +0100

    Comments + introduce variables

--- tennis/modules/tennis2.py
+++ tennis/modules/tennis2.py
@@ -16,18 +16,30 @@ class TennisGame2:
     
     def score(self):
         points = {0:"Love", 1:"Fifteen", 2:"Thirty", 3:"Forty"}
-        if (self.points_player1 < 4 and self.points_player2 < 4) and (self.points_player1 + self.points_player2 < 6):
+    
+        #Define which player has the most points
+        if self.points_player1 > self.points_player2:
+            player = self.player1
+        elif self.points_player1 < self.points_player2:
+            player = self.player2
+
+        #Nobody won and we didn't get to advantages yet
+        NoEquailityAtForty = (self.points_player1 + self.points_player2 != 6)
+        PointsUnder4 = (self.points_player1 < 4 and self.points_player2 < 4)
+        if PointsUnder4 and NoEquailityAtForty :
            score_player1 = points[self.points_player1]
            score_player2 = points[self.points_player2]
+    
            if (self.points_player1 == self.points_player2):
                return score_player1 + "-All"
            else:
                return score_player1 + "-" + score_player2
+           
+        #Deuce, Advantage or Win
         else:
-            score = self.player1 if self.points_player1 > self.points_player2 else self.player2
             if (self.points_player1 == self.points_player2):
                 return "Deuce"
             elif abs(self.points_player1 - self.points_player2) == 1:
-                return "Advantage " + score
+                return "Advantage " + player
             else:
-                return "Win for " + score
+                return "Win for " + player
