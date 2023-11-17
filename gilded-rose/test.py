796895511d01e261df592bf6a9581c9502c98332
Théo FURST <tfurst@adflux.net>
2023-11-17 15:51:20 +0100

    Tests on quality and sell_in are working

--- gilded-rose/test.py
+++ gilded-rose/test.py
@@ -17,9 +17,18 @@ test_cases_qual = {
     "expect": [19, 1, 6, 80, 80, 21, 50, 50, 4]
 }
 
-#test_cases_sellin = {
-    
-#}
+test_cases_sellin = {
+    "it": [Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6)],
+    "expect": [9, 1, 4, 0, -1, 14, 9, 4, 2]
+}
 
 class GildedRoseTest(unittest.TestCase):
     
@@ -30,6 +39,14 @@ class GildedRoseTest(unittest.TestCase):
         
         for i, expected in enumerate(test_cases_qual["expect"]):
             self.assertEquals(expected, gilded_rose.items[i].quality)
+            
+    # Unit Test 2
+    def test_sellin(self):
+        gilded_rose = GildedRose(test_cases_sellin["it"])
+        gilded_rose.update_quality()
+        
+        for i, expected in enumerate(test_cases_sellin["expect"]):
+            self.assertEquals(expected, gilded_rose.items[i].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
