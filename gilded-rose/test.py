e33ff9730a2a662469d392bfab11c562cc368196
elisagrs <elisa.grassi@businessdecision.be>
2023-11-17 16:34:23 +0100

    edit update_quality

--- gilded-rose/test.py
+++ gilded-rose/test.py
@@ -3,12 +3,70 @@ import unittest
 
 from modules.gilded_rose import Item, GildedRose
 
+test_cases = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="+5 Dexterity Vest", sell_in=-1, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Aged Brie", sell_in=2, quality=60),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=50),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=40),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6), 
+        Item(name="Elixir of the Mongoose", sell_in=-4, quality=4),
+    ]
+
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+    def test_name(self):
+        gilded_rose = GildedRose(test_cases)
+        items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="+5 Dexterity Vest", sell_in=-1, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Aged Brie", sell_in=2, quality=60),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=50),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=40),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6), 
+        Item(name="Elixir of the Mongoose", sell_in=-1, quality=4),
+    ]
+        
+        for (idx, item )  in enumerate(  gilded_rose.items ) :
+            self.assertEqual(item.name, items[idx].name)
+
+    def test_quality(self):
+        items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=19),
+        Item(name="+5 Dexterity Vest", sell_in=-1, quality=18),
+        Item(name="Aged Brie", sell_in=1, quality=1),
+        Item(name="Aged Brie", sell_in=2, quality=60),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=6),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=50),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=14, quality=21),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=50),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=50),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=43),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=5), 
+        Item(name="Elixir of the Mongoose", sell_in=-1, quality=2),
+    ]
+        gilded_rose = GildedRose(test_cases)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        for (idx, item )  in enumerate(  gilded_rose.items ) :
+            self.assertEqual(item.quality, items[idx].quality)
 
 if __name__ == '__main__':
     unittest.main()
