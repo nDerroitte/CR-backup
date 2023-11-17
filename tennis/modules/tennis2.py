b98a7c703ef405c91682e977c4e01bbdce4b6ee3
mdupret <maureen.dupret@businessdecision.be>
2023-11-17 14:08:17 +0100

    Correction

--- tennis/modules/tennis2.py
+++ tennis/modules/tennis2.py
@@ -6,6 +6,7 @@ class TennisGame2:
         self.player2 = player2Name
         self.points_player1 = 0
         self.points_player2 = 0
+        self.RESULT = ["Love", "Fifteen", "Thirty", "Forty"]
         
     def won_point(self, playerName):
         if playerName == self.player1:
@@ -13,33 +14,31 @@ class TennisGame2:
         else:
             self.points_player2 += 1
     
+    def current_winner(self):
+        if self.points_player1 > self.points_player2:
+            return self.player1
+        else:
+            return self.player2
+
     
     def score(self):
-        points = {0:"Love", 1:"Fifteen", 2:"Thirty", 3:"Forty"}
-    
-        #Define which player has the most points
-        if self.points_player1 > self.points_player2:
-            player = self.player1
-        elif self.points_player1 < self.points_player2:
-            player = self.player2
+        #Same score 
+        if (self.points_player1 == self.points_player2):
+            if self.points_player1 < 3:
+                return self.RESULT[self.points_player1] + "-All"
+            else:
+                return "Deuce"
 
         #Nobody won and we didn't get to advantages yet
-        NoEquailityAtForty = (self.points_player1 + self.points_player2 != 6)
         PointsUnder4 = (self.points_player1 < 4 and self.points_player2 < 4)
-        if PointsUnder4 and NoEquailityAtForty :
-           score_player1 = points[self.points_player1]
-           score_player2 = points[self.points_player2]
-    
-           if (self.points_player1 == self.points_player2):
-               return score_player1 + "-All"
-           else:
-               return score_player1 + "-" + score_player2
+        if PointsUnder4: 
+           score_player1 = self.RESULT[self.points_player1]
+           score_player2 = self.RESULT[self.points_player2]
+           return score_player1 + "-" + score_player2
            
-        #Deuce, Advantage or Win
+        #Advantage or Win
         else:
-            if (self.points_player1 == self.points_player2):
-                return "Deuce"
-            elif abs(self.points_player1 - self.points_player2) == 1:
-                return "Advantage " + player
+            if abs(self.points_player1 - self.points_player2) == 1:
+                return "Advantage " + self.current_winner()
             else:
-                return "Win for " + player
+                return "Win for " + self.current_winner()
