e960d15fdd52e9a244e9aecdc5a2d487afe734c8
Théo FURST <tfurst@adflux.net>
2023-11-17 10:39:42 +0100

    Modify the formula of pointDiff

--- tennis/modules/tennis2.py
+++ tennis/modules/tennis2.py
@@ -21,5 +21,5 @@ class TennisGame2:
             if (self.p1 == self.p2):
                 return "Deuce"
             s = self.p1N if self.p1 > self.p2 else self.p2N
-            pointDiff = pow(self.p1-self.p2, 2)
+            pointDiff = self.p1-self.p2
             return "Advantage " + s if (pointDiff == 1) else "Win for " + s
