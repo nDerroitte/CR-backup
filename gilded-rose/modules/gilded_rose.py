d555160e7bc101fe37c02fc1938a0338ffb1270e
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-20 11:04:50 +0100

    plain object function

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -8,42 +8,40 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def _aged_brie(self):
-        for item in self.items:
-            if item.name == "Aged Brie":
-                item.sell_in = item.sell_in - 1
-                if item.sell_in >= 0:
-                    item.quality = min(item.quality + 1, 50)
-                elif item.sell_in < 0:
-                    item.quality = min(item.quality + 2, 50)
+    def _aged_brie(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in >= 0:
+            item.quality = min(item.quality + 1, 50)
+        elif item.sell_in < 0:
+            item.quality = min(item.quality + 2, 50)
 
-    def _backstage_passes(self):
-        for item in self.items:
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.sell_in = item.sell_in - 1
-                if item.quality <= 50:
-                    if item.sell_in > 10:
-                        item.quality = min(item.quality + 1, 50)
-                    elif 5 < item.sell_in <= 10:
-                        item.quality = min(item.quality + 2, 50)
-                    elif -1 < item.sell_in <= 5:
-                        item.quality = min(item.quality + 3, 50)
-                    elif item.sell_in < 0:
-                        item.quality = 0
+    def _backstage_passes(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in >= 10:
+            item.quality = min(item.quality + 1, 50)
+        elif 5 <= item.sell_in < 10:
+            item.quality = min(item.quality + 2, 50)
+        elif -1 < item.sell_in < 5:
+            item.quality = min(item.quality + 3, 50)
+        elif item.sell_in < 0:
+            item.quality = 0
+
+    def _plain_object(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.quality > 0:
+                item.quality = item.quality - 1
 
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
-                self._aged_brie()
+                self._aged_brie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self._backstage_passes()
-            elif item.name not in ["Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
+                self._backstage_passes(item)
+            elif item.name not in exception_objects:
+                self._plain_object(item)
 
 
 class Item:
