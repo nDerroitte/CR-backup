012b3144287a66034a4e2f80780064a36a58d627
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-20 10:46:26 +0100

    added backstage passes function

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -17,35 +17,33 @@ class GildedRose(object):
                 elif item.quality < 49 and item.sell_in < 0:
                     item.quality = item.quality + 2
 
+    def _backstage_passes(self):
+        for item in self.items:
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.sell_in = item.sell_in - 1
+                if item.quality <= 50:
+                    if item.sell_in > 10:
+                        item.quality = min(item.quality + 1, 50)
+                    elif 5 < item.sell_in <= 10:
+                        item.quality = min(item.quality + 2, 50)
+                    elif -1 < item.sell_in <= 5:
+                        item.quality = min(item.quality + 3, 50)
+                    elif item.sell_in < 0:
+                        item.quality = 0
+
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
                 self._aged_brie()
-            elif item.name not in ["Sulfuras, Hand of Ragnaros", "Aged Brie"]:
-                if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if item.quality > 0:
-                        item.quality = item.quality - 1
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                            if item.sell_in < 11:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
-                            if item.sell_in < 6:
-                                if item.quality < 50:
-                                    item.quality = item.quality + 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self._backstage_passes()
+            elif item.name not in ["Sulfuras, Hand of Ragnaros", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert"]:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
                 item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.name != "Aged Brie":
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                            if item.quality > 0:
-                                item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - item.quality
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+                    if item.quality > 0:
+                        item.quality = item.quality - 1
 
 
 class Item:
