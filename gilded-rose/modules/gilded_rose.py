05e4b3942705b093bddc27be258c144a5060ba5d
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-20 12:05:54 +0100

    small update

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -56,7 +56,7 @@ class BackstagePasses(Item):
 class PlainObject(Item):
     def update_quality(self, item):
         item.quality = max(item.quality - 1, 0)
-        item.sell_in += - 1
+        item.sell_in -= 1
         if item.sell_in < 0:
             if item.quality > 0:
-                item.quality += -1
+                item.quality -= 1
