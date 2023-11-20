2b8b81ac7238fa8f7d9c14ed8075cd4c16e9b247
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-20 12:00:50 +0100

    class method for plain objects and backstage passes

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -8,40 +8,17 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def _aged_brie(self, item):
-        item.sell_in += - 1
-        if item.sell_in >= 0:
-            item.quality = min(item.quality + 1, 50)
-        elif item.sell_in < 0:
-            item.quality = min(item.quality + 2, 50)
-
-    def _backstage_passes(self, item):
-        item.sell_in += - 1
-        if item.sell_in >= 10:
-            item.quality = min(item.quality + 1, 50)
-        elif 5 <= item.sell_in < 10:
-            item.quality = min(item.quality + 2, 50)
-        elif -1 < item.sell_in < 5:
-            item.quality = min(item.quality + 3, 50)
-        elif item.sell_in < 0:
-            item.quality = 0
-
-    def _plain_object(self, item):
-        item.quality = max(item.quality - 1, 0)
-        item.sell_in += - 1
-        if item.sell_in < 0:
-            if item.quality > 0:
-                item.quality += -1
-
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
                 AgedBrie(item.name, item.sell_in, item.quality) \
                     .update_quality(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self._backstage_passes(item)
+                BackstagePasses(item.name, item.sell_in, item.quality) \
+                    .update_quality(item)
             elif item.name not in exception_objects:
-                self._plain_object(item)
+                PlainObject(item.name, item.sell_in, item.quality) \
+                    .update_quality(item)
 
 
 class Item:
@@ -63,3 +40,23 @@ class AgedBrie(Item):
             item.quality = min(item.quality + 2, 50)
 
 
+class BackstagePasses(Item):
+    def update_quality(self, item):
+        item.sell_in += - 1
+        if item.sell_in >= 10:
+            item.quality = min(item.quality + 1, 50)
+        elif 5 <= item.sell_in < 10:
+            item.quality = min(item.quality + 2, 50)
+        elif -1 < item.sell_in < 5:
+            item.quality = min(item.quality + 3, 50)
+        elif item.sell_in < 0:
+            item.quality = 0
+
+
+class PlainObject(Item):
+    def update_quality(self, item):
+        item.quality = max(item.quality - 1, 0)
+        item.sell_in += - 1
+        if item.sell_in < 0:
+            if item.quality > 0:
+                item.quality += -1
