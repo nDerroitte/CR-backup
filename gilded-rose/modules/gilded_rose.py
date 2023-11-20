23797714cdc38bf0719be826e6c0a340425fc42a
Lucien Jacques de Dixmude <lucien.jacquesdedixmude@businessdecision.be>
2023-11-20 11:17:03 +0100

    refactoring

--- gilded-rose/modules/gilded_rose.py
+++ gilded-rose/modules/gilded_rose.py
@@ -9,14 +9,14 @@ class GildedRose(object):
         self.items = items
 
     def _aged_brie(self, item):
-        item.sell_in = item.sell_in - 1
+        item.sell_in += - 1
         if item.sell_in >= 0:
             item.quality = min(item.quality + 1, 50)
         elif item.sell_in < 0:
             item.quality = min(item.quality + 2, 50)
 
     def _backstage_passes(self, item):
-        item.sell_in = item.sell_in - 1
+        item.sell_in += - 1
         if item.sell_in >= 10:
             item.quality = min(item.quality + 1, 50)
         elif 5 <= item.sell_in < 10:
@@ -27,12 +27,11 @@ class GildedRose(object):
             item.quality = 0
 
     def _plain_object(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-        item.sell_in = item.sell_in - 1
+        item.quality = max(item.quality - 1, 0)
+        item.sell_in += - 1
         if item.sell_in < 0:
             if item.quality > 0:
-                item.quality = item.quality - 1
+                item.quality += -1
 
     def update_quality(self):
         for item in self.items:
